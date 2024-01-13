80bc73eec40cbe062b8b05352b6293648b015a89
giraffetree <giraffetree1@gmail.com>
2024-01-13 12:50:13 +0800

    init;

---
+++ src/main/java/cn/giraffetree/boom/BoomBoomJavaApplication.java
@@ -0,0 +1,13 @@
+package cn.giraffetree.boom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomBoomJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoomBoomJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/giraffetree/boom/BoomBoomJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.giraffetree.boom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoomBoomJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
